2109e8096439d592964de6d91faf586055d229c1
Antonio Bernardino da Silva <contato@antoniobsilva.com.br>
2024-10-02 13:41:47 -0300

    chore: remove .env

--- src/shared/env/env.ts
+++ src/shared/env/env.ts
@@ -4,11 +4,11 @@ export const envSchema = z.object({
   DATABASE_URL: z.string({ required_error: 'A DATABASE_URL deve ser adicionada' }).url(),
   AUTH_SECRET: z.string()
     .min(10, 'AUTH_SECRET deve ter pelo menos 10 caracteres')
-    .default('u3vYjYnjN5GPToFjnjaU9RQPb6ALn6ano5UtMHdAvl4='),
+    .default('secret'),
   PORT: z.string().default("3000"),
   NODE_ENV: z.enum(['development', 'production', 'test', 'homolog']).default('development'),
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string()
+  GOOGLE_CLIENT_ID: z.string().default('secret'),
+  GOOGLE_CLIENT_SECRET: z.string().default('secret')
 });
 
 export type EnvVars = z.infer<typeof envSchema>;
