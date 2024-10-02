65d4d261b1317d9dc77368366e9487cf0fce5343
Antonio Bernardino da Silva <contato@antoniobsilva.com.br>
2024-10-02 14:27:05 -0300

    chore: update secret name

--- src/shared/env/env.ts
+++ src/shared/env/env.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   DATABASE_URL: z.string({ required_error: 'A DATABASE_URL deve ser adicionada' }).url(),
-  AUTH_SECRET: z.string()
-    .min(10, 'AUTH_SECRET deve ter pelo menos 10 caracteres')
-    .default('secret'),
+  NEXTAUTH_SECRET: z.string()
+    .min(10, 'NEXTAUTH_SECRET deve ter pelo menos 10 caracteres')
+    .default('secret1234'),
   PORT: z.string().default("3000"),
   NODE_ENV: z.enum(['development', 'production', 'test', 'homolog']).default('development'),
   GOOGLE_CLIENT_ID: z.string().default('secret'),
